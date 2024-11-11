4685b67165836532b381ea84555615531d8536eb
Doris Duaka <dorisduaka@gmail.com>
2024-11-11 16:55:04 +0000

    Weather app
    started the project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+let hour = prompt("What is the current hour?");
+if (hour < 12) {
+  alert("Good Morning!☀️");
+} else if (hour < 18) {
+  alert("Good Afternoon!😎");
+} else {
+  alert("Good Evening! 🌙");
+}
